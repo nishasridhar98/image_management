bc5f241599f9aa71f3beeaba1af4aa083877d22e
Nisha <nisha.sridhar1998@gmail.com>
2018-08-07 20:08:22 +0530

    added code to convert images to black nd white

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageFilter
 import os
 
 while True:
-	dest = input("Enter the full path to where you want to store the images: ")
 	print("1. Rotate Image /n 2. Convert Image to Black and White /n 3. Blur the Image /n 4. Change the size")
 	num = int(input("Enter the index number what you want to do: "))
 	# Open all the images present in the folder where images are stored
@@ -12,6 +11,14 @@ while True:
 		# Add code to perform rotate operation and save the images
 	if num == 2:
 		# Add code to convert images to black and white
+		dest = input("Enter the full path to where you want to store the images: ")
+		for f in os.listdir("Images"):#listdir gives the list of files in that folder
+			if f.endswith('.jpg'):
+				im = Image.open("Images/"+ str(f))#y is str(f) used??
+				filename, file_extn = os.path.splitext(f)#splitext splits the filename nd extension
+				im=im.convert(mode='L')
+				im.save(f"{dest}/{filename}.jpg")
+
 	if num == 3:
 		# Add code to blur the images
 		dest = input("Enter the full path to where you want to store the images: ")
